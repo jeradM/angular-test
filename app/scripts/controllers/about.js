a932f2044d1cf6421596601ba8dea97ef3d95f3b
Jerad Meisner <jerad.meisner@gmail.com>
2015-11-07 18:15:13 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularExample1App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularExample1App
+ */
+angular.module('angularExample1App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularExample1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularExample1App
+ */
+angular.module('angularExample1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
